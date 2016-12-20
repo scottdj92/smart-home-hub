3819a1d3e3600dd7239efb0d732e09e0f5758199
Scott Jones <scottdj92@gmail.com>
2016-12-20 10:55:26 -0500

    add a few links to nav

--- src/app/Nav/Nav.jsx
+++ src/app/Nav/Nav.jsx
@@ -5,7 +5,9 @@ class Nav extends React.Component {
         return (
             <div className='main-nav'>
                 <ul>
-                    <li>main nav 1</li>
+                    <li><a>Devices</a></li>
+                    <li><a>Connection Status</a></li>
+                    <li><a>Permissions</a></li>
                 </ul>
             </div>
         )
