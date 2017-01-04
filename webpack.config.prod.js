e6b3b9e293ddfd58680492149e229d66f1f7acff
Scott Jones <scottdj92@gmail.com>
2017-01-04 13:18:37 -0500

    Add prod webpack config. Sass now works on dev mode

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,7 @@
-const webpack = require('webpack');
-const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
-
-const prod = require('./webpack.config.prod');
+const path = require('path');
 
 const PATHS = {
     app: path.resolve(__dirname, 'src'),
@@ -11,49 +9,46 @@ const PATHS = {
     assets: path.resolve(__dirname, 'dist/assets')
 };
 
-const common = merge(
+const prod = merge(
     {
         context: PATHS.app,
-        entry: {
-            app: PATHS.app + '/index.jsx'
-        },
-        output: {
-            path: PATHS.build,
-            filename: 'bundle.js'
-        },
+        filename: 'bundle.js',
         plugins: [
             new HtmlWebpackPlugin({
                 template: 'index.html'
+            }),
+            new ExtractTextWebpackPlugin({
+                filename: 'bundle.css'
             })
         ],
         resolve: {
             modules: ['node_modules'],
-            extensions: ['.css', '.js', '.jsx', '.scss', '.json']
+            extensions: ['.css', '.js', '.json', '.jsx', '.scss']
         },
         module: {
             rules: [
                 {
                     test: /\.(js|jsx)$/,
                     include: PATHS.app,
-                    use: ['react-hot-loader', 'babel-loader']
+                    use: ['babel-loader']
                 },
                 {
                     test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
+                    use: ExtractTextWebpackPlugin.extract({
+                        fallbackLoader: 'style-loader',
+                        loader: 'css-loader'
+                    })
                 },
                 {
                     test: /\.scss$/,
-                    use: ['style-loader', 'css-loader', 'sass-loader']
+                    use: ExtractTextWebpackPlugin.extract({
+                        fallbackLoader: 'style-loader',
+                        loader: 'sass-loader?sourceMap'
+                    })
                 }
             ]
         }
     }
 );
 
-module.exports = function(env) {
-    if (env === 'development') {
-        return merge(common);
-    } else {
-        return merge(prod);
-    }
-};
+module.exports = prod;
